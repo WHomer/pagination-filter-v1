88437ba9a76bafb266ce6c66e550f1d0513a6ce9
WHomer <billyhomeriii@gmail.com>
2016-07-25 16:05:06 -0500

    Updates
    Completed various updates on the code.

--- js/app.js
+++ js/app.js
@@ -11,11 +11,13 @@ var students = document.getElementsByClassName('student-item');
 var student = ""; //declare variable
 var studentsPerPage = 10; //Number of students to display per page
 var studentCount = students.length; //How many students are in the list
-var pageCount = Math.ceil(studentCount/studentsPerPage);
+var pageButtonCount;
 
 var pageButton = document.createElement('button');
 var pageNumber = document.createTextNode('1');
 var pageButtonHolder = document.getElementById('pageButtons');
+
+var studentArray = []; //temp storage of students
 
 //Creates button for each group of students
 function createButton(currentPage) {
@@ -23,64 +25,94 @@ function createButton(currentPage) {
   pageButton = document.createElement('button');
   //give it the text value of the page number
   pageNumber = document.createTextNode(currentPage);
-  //assign the class name used for styleing
+  //assign the class name used for styling
   pageButton.className = 'studentPageButtons';
   //add the page number for the id
   pageButton.id = 'pg' + currentPage;
   pageButton.appendChild(pageNumber);
   pageButtonHolder.appendChild(pageButton);
   //create a click listener to run the displayStudents function.
-  pageButton.addEventListener('click', function(){displayStudents(currentPage)});
+  pageButton.addEventListener('click', function(){displayStudents(currentPage);});
+  pageButtonCount = currentPage;
 }
+
 //Displays students to screen on load as well as when button is pressed.
-function displayStudents(pageNumber) {
-  //remove all currently displayed students
-  for (i = 0; i < studentCount; i++){
-    students[i].style.display = 'none';
-  }
+function displayStudents(pageNumber){
+  hideStudents(); //Hide all currently displayed students
   //Current page number  x students per page minus students per page to get you the lowest value
   displayRecords = pageNumber * studentsPerPage - studentsPerPage;
-  //BUG: not set yet to stop once max records are displayed. not causing any visual errors
-  for (i = 0; i < studentsPerPage; i++){
-    student = students[displayRecords];
-    //increments by 1 to display next record.
-    displayRecords++;
-    //sets style of display to block, default is none
-    student.style.display = 'block';
+  if (studentArray.length > 0) {
+    //display current users per page
+    for (i = displayRecords; i < (studentsPerPage+displayRecords); i++){
+      //sets position of n to position of student-item class
+      var n = studentArray[i];
+      //gets the correct student-item class
+      student = document.getElementsByClassName('student-item')[n];
+      //displays the student-item class
+      if (student != null) {
+        student.style.display = 'block';
+      }
+    }
+  }else{
+    //if no records display message.
+    console.log('No Students to Display');
+    document.getElementById('record').innerHTML = 'No Students to Display';
+  }
+  //Display page buttons
+  var pageCount = Math.ceil(studentArray.length/studentsPerPage);
+  if (document.getElementById('pg1') != null) {
+    removePageButtons();
+  }
+  for (i = 1; i <= pageCount; i++) {
+    createButton(i);
   }
 }
 
-//Testing out Searching for name's on div's
-//TESTING BELOW
-function searchStudents() {
-  var search = 'ra';
+function hideStudents() {
   //remove all currently displayed students
   for (i = 0; i < studentCount; i++){
     students[i].style.display = 'none';
   }
-  for (i =0; i < studentCount; i++){
-    var studentName = document.getElementsByTagName('H3')[i].innerHTML;
-    var n = studentName.search(/search/i);
-    if (n > -1){
-      student = students[i];
-      //sets style of display to block, default is none
-      student.style.display = 'block';
-      console.log('Record: ' + i + ' Space: ' + n);
-    }
+  //Clear div #record
+  document.getElementById('record').innerHTML = '';
+}
 
+function removePageButtons() {
+  //Clear current buttons
+  for (i = 1; i <= pageButtonCount; i++) {
+    pageButtonHolder.removeChild(document.getElementById('pg' + i));
   }
 }
 
-//TESTING ABOVE
 
-//creates a new button for each record in pageCount
-for (i = 1; i <= pageCount; i++ ) {
-  currentPage = i;
-  createButton(currentPage);
+function searchStudents(searchString) {
+  hideStudents(); //Hide all currently displayed students
+  studentArray = []; //Clear studentArray
+  if (searchString != null) {
+    for (i =0; i < studentCount; i++){
+      var studentName = document.getElementsByTagName('H3')[i].innerHTML;
+      var n = studentName.search(searchString);
+      if (n > -1) {
+        studentArray.push(i);
+      }
+    }
+  } else {
+    for (i =0; i < studentCount; i++){
+      studentArray.push(i);
+    }
+  }
+  displayStudents(1);
 }
 
+function runSearch() {
+  var searchValue = document.getElementById('searchInput').value;
+  searchValue = new RegExp(searchValue, 'i');
+  searchStudents(searchValue);
+}
 
+searchStudents(); // Run the searchStudents function.
+//Adds event listener to the search button
+document.getElementById('search').addEventListener('click', runSearch);
 
-displayStudents(1); //value of 1 is as if it was loading the first page.
-
-searchStudents();
+//On keyup run search.
+document.getElementById('searchInput').addEventListener('keyup', runSearch);
